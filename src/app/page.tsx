e5054a14e953057e7d13478b158a5a75455744aa
999Dome <124059485+999Dome@users.noreply.github.com>
2025-06-06 16:55:38 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <p>kevin ist kernbehindert yeah</p>;
+  return <p>kevin ist kernbehindert yeah gang shit xeah xeahnpm install @supabase/supabase-js</p>;
 }
